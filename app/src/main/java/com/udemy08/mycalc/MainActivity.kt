1d031866aa35702f1ae08db20519e194ff9c15cc
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-01-30 23:43:28 +0100

    Add binding and other button functionality

--- app/src/main/java/com/udemy08/mycalc/MainActivity.kt
+++ app/src/main/java/com/udemy08/mycalc/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.udemy08.mycalc
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.EditText
@@ -11,45 +12,74 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
+import com.udemy08.mycalc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         initListeners()
     }
 
-     private fun initListeners() {
-        findViewById<MaterialButton>(R.id.btnAdd).setOnClickListener {
-            val firstInput = findViewById<TextInputEditText>(R.id.edtFirstVal)
-            val secondInput = findViewById<TextInputEditText>(R.id.edtSecondVal)
-            if (TextUtils.isEmpty(firstInput.text)){
-                firstInput.error=getString(R.string.str_enter_first_number)
-           } else if (TextUtils.isEmpty(secondInput.text)){
-                secondInput.error=getString(R.string.str_enter_second_number)
-           } else {
-                val edtResult = Integer.valueOf(firstInput.text.toString()) + secondInput.text.toString().toInt()
-                findViewById<TextView>(R.id.txtRes).text = edtResult.toString()
+    private fun initListeners() {
+        binding?.apply {
+            btnAdd.setOnClickListener {
+                if (TextUtils.isEmpty(edtFirstVal.text)) {
+                    edtFirstVal.error = getString(R.string.str_enter_first_number)
+                } else if (TextUtils.isEmpty(edtSecondVal.text)) {
+                    edtSecondVal.error = getString(R.string.str_enter_second_number)
+                } else {
+                    val edtResult =
+                        Integer.valueOf(edtFirstVal.text.toString()) + edtSecondVal.text.toString().toInt()
+                    txtRes.text = edtResult.toString()
+                }
             }
-        }
 
-        findViewById<MaterialButton>(R.id.btnMin).setOnClickListener {
-            val firstInput = findViewById<TextInputEditText>(R.id.edtFirstVal)
-            val secondInput = findViewById<TextInputEditText>(R.id.edtSecondVal)
-            if (TextUtils.isEmpty(firstInput.text)){
-                firstInput.error=getString(R.string.str_enter_first_number)
-            } else if (TextUtils.isEmpty(secondInput.text)){
-                secondInput.error=getString(R.string.str_enter_second_number)
-            } else {
-                val edtResult = Integer.valueOf(firstInput.text.toString()) - secondInput.text.toString().toInt()
-                findViewById<TextView>(R.id.txtRes).text = edtResult.toString()
+            btnMin.setOnClickListener {
+                if (TextUtils.isEmpty(edtFirstVal.text)) {
+                    edtFirstVal.error = getString(R.string.str_enter_first_number)
+                } else if (TextUtils.isEmpty(edtSecondVal.text)) {
+                    edtSecondVal.error = getString(R.string.str_enter_second_number)
+                } else {
+                    val edtResult =
+                        Integer.valueOf(edtFirstVal.text.toString()) - edtSecondVal.text.toString().toInt()
+                    txtRes.text = edtResult.toString()
+                }
+            }
+
+            btnMulti.setOnClickListener {
+                if (TextUtils.isEmpty(edtFirstVal.text)) {
+                    edtFirstVal.error = getString(R.string.str_enter_first_number)
+                } else if (TextUtils.isEmpty(edtSecondVal.text)) {
+                    edtSecondVal.error = getString(R.string.str_enter_second_number)
+                } else {
+                    val edtResult = 
+                        Integer.valueOf(edtFirstVal.text.toString()) * edtSecondVal.text.toString().toInt()
+                    txtRes.text = edtResult.toString()
+                }
+            }
+
+            btnDiv.setOnClickListener {
+                if (TextUtils.isEmpty(edtFirstVal.text)) {
+                    edtFirstVal.error = getString(R.string.str_enter_first_number)
+                } else if (TextUtils.isEmpty(edtSecondVal.text)) {
+                    edtSecondVal.error = getString(R.string.str_enter_second_number)
+                } else {
+                    val edtResult =
+                        Integer.valueOf(edtFirstVal.text.toString()) / edtSecondVal.text.toString().toInt()
+                    txtRes.text = edtResult.toString()
+                }
             }
         }
     }
